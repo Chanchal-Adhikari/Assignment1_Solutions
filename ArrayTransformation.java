ac1458dde2d7b07731a6e52e93ca0a45fab04730
Chanchal Adhikari <chanchaladhikari21@gmail.com>
2026-05-05 11:32:18 +0530

    Update ArrayTransformation.java

--- ArrayTransformation.java
+++ ArrayTransformation.java
@@ -4,34 +4,34 @@ public class ArrayTransformation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] A = new int[n];
+        int n =sc.nextInt();
+        int[] A =new int[n];
 
-        for (int i = 0; i < n; i++) {
-            A[i] = sc.nextInt();
+        for (int i=0;i<n;i++){
+            A[i] =sc.nextInt();
         }
 
-        int K = sc.nextInt();
+        int K =sc.nextInt();
 
-        for (int i = 1; i < n; i++) {
-            if ((A[i] - A[0]) % K != 0) {
+        for (int i=1;i<n;i++) {
+            if ((A[i]-A[0])%K!=0){
                 System.out.println(-1);
                 return;
             }
         }
 
         int[] B = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0;i < n;i++){
             B[i] = (A[i] - A[0]) / K;
         }
 
         Arrays.sort(B);
 
-        int median = B[n / 2];
+        int median = B[n/2];
 
         int operations = 0;
-        for (int x : B) {
-            operations += Math.abs(x - median);
+        for(int x : B){
+           operations += Math.abs(x - median);
         }
 
         System.out.println(operations);
